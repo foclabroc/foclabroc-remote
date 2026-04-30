e81be3cba99dcac35ff09493eb847072ecc2292a
foclabroc <foclabroc@hotmail.fr>
2026-04-30 14:42:19 +0200

    2.7++

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -16,7 +16,7 @@ import 'quiz_screen.dart';
 import 'breakout_screen.dart';
 import 'links_screen.dart';
 
-const kAppVersion = '2.6-FR';
+const kAppVersion = '2.7-FR';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
